06fa288bf9602e76d22e41abfac804044f5dc88d
David Nguyen <davidnguyen234@gmail.com>
2021-05-13 11:55:10 -0700

    ui layout - buttons with toast click event

--- app/src/main/java/com/dlnsoft/adroidapplication/MainActivity.java
+++ app/src/main/java/com/dlnsoft/adroidapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.dlnsoft.adroidapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showCitiesToast(View view) {
+        Toast t = Toast.makeText(this, "Cities", Toast.LENGTH_SHORT);
+        t.show();
+    }
+
+    public void showFoodToast(View view) {
+        Toast t = Toast.makeText(this, "Food", Toast.LENGTH_SHORT);
+        t.show();
+    }
+
+    public void showMoviesToast(View view) {
+        Toast t = Toast.makeText(this, "Movies", Toast.LENGTH_SHORT);
+        t.show();
+    }
+
+    public void showMusicToast(View view) {
+        Toast t = Toast.makeText(this, "Music", Toast.LENGTH_SHORT);
+        t.show();
+    }
+
+    public void showParksToast(View view) {
+        Toast t = Toast.makeText(this, "Parks", Toast.LENGTH_SHORT);
+        t.show();
+    }
+
+    public void showTrafficToast(View view) {
+        Toast t = Toast.makeText(this, "Traffic", Toast.LENGTH_SHORT);
+        t.show();
     }
 }

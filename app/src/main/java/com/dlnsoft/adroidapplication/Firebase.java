3ad9539a0e16fa77f903a9023f7b1b0a31823d64
David Nguyen <davidnguyen234@gmail.com>
2021-06-18 08:07:18 -0700

    add firebase activity

---
+++ app/src/main/java/com/dlnsoft/adroidapplication/Firebase.java
@@ -0,0 +1,132 @@
+package com.dlnsoft.adroidapplication;
+
+import android.content.Context;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+
+public class Firebase extends AppCompatActivity {
+
+    private static final String TAG = Firebase.class.getSimpleName();
+
+    private DatabaseReference myRef;
+
+    ListView userList;
+    UserListAdapter listAdapter;
+    ArrayList<User> userData = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase);
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        userList = findViewById(R.id.userList);
+        listAdapter = new UserListAdapter(this, userData);
+        userList.setAdapter(listAdapter);
+
+        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+        myRef = mDatabase.getReference("users");
+        Query members = myRef.orderByChild("updated");
+
+        // update database
+        assert currentUser != null;
+        writeNewUser(currentUser.getUid(), currentUser.getDisplayName(), currentUser.getEmail());
+
+        // get list of users
+        members.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d("FIREBASE", "onDataChange");
+                for (DataSnapshot userSnapshot: dataSnapshot.getChildren()) {
+                    Object username = userSnapshot.child("username").getValue();
+                    if (username != null) {
+                        User user = new User(username.toString(), userSnapshot.child("email").getValue().toString(), userSnapshot.child("updated").getValue().toString());
+                        userData.add(user);
+                    }
+                }
+                // Firebase doesn't support sort in descending order. Have to reverse in java
+                Collections.reverse(userData);
+                listAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadUsers:onCancelled", databaseError.toException());
+                // ...
+            }
+        });
+
+    }
+
+    private void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email, new Date().toString());
+        myRef.child(userId).setValue(user);
+    }
+
+    @IgnoreExtraProperties
+    public static class User {
+
+        public String username;
+        public String email;
+        public String updated;
+
+        public User() {
+            // Default constructor required for calls to DataSnapshot.getValue(User.class)
+        }
+
+        private User(String username, String email, String updated) {
+            this.username = username;
+            this.email = email;
+            this.updated = updated;
+        }
+
+    }
+
+    private static class UserListAdapter extends ArrayAdapter<User> {
+        private final Context context;
+        private final ArrayList<User> values;
+
+        private UserListAdapter(Context context, ArrayList<User> values) {
+            super(context, 0, values);
+            this.context = context;
+            this.values = values;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.list_item, parent, false);
+            TextView title = rowView.findViewById(R.id.item_title);
+            title.setText(values.get(position).username);
+            TextView subtitle = rowView.findViewById(R.id.item_subtitle);
+            subtitle.setText(String.format("Updated: %s", values.get(position).updated));
+
+            return rowView;
+        }
+    }
+}
--- app/src/main/java/com/dlnsoft/adroidapplication/MainActivity.java
+++ app/src/main/java/com/dlnsoft/adroidapplication/MainActivity.java
@@ -2,17 +2,114 @@ package com.dlnsoft.adroidapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText usernameInput;
+    private EditText emailInput;
+    private EditText passwordInput;
+    SharedPreferences sharedPreferences;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usernameInput = findViewById(R.id.userName);
+        emailInput = findViewById(R.id.email);
+        passwordInput = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        sharedPreferences = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+        usernameInput.setText(getData("userName"));
+        emailInput.setText(getData("email"));
+        passwordInput.setText(getData("password"));
+    }
+
+    private void signIn() {
+        Log.d("FIREBASE", "signIn");
+
+        // 1 - validate display name, email, and password entries
+        String userName = usernameInput.getText().toString();
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        Log.d("Password", "Error");
+        Log.d("UserName", userName);
+
+        if (userName.isEmpty()) {
+            usernameInput.setError("Enter username");
+            usernameInput.requestFocus();
+            return;
+        }
+        if (email.isEmpty()) {
+            emailInput.setError("Enter email");
+            emailInput.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            passwordInput.setError("Enter password");
+            passwordInput.requestFocus();
+            return;
+        }
+
+        // 2 - save valid entries to shared preferences
+        saveData("userName", userName);
+        saveData("email", email);
+        saveData("password", password);
+
+        // 3 - sign into Firebase
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    Log.d("FIREBASE", "signIn:onComplete:" + task.isSuccessful());
+
+                    if (task.isSuccessful()) {
+                        // update profile. display name is the value entered in UI
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(userName)
+                                .build();
+
+                        assert user != null;
+                        user.updateProfile(profileUpdates)
+                                .addOnCompleteListener(task1 -> {
+                                    if (task1.isSuccessful()) {
+                                        Log.d("FIREBASE", "User profile updated.");
+                                        // Go to FirebaseActivity
+                                        startActivity(new Intent(MainActivity.this, Firebase.class));
+                                    }
+                                });
+
+                    } else {
+                        Log.d("FIREBASE", "sign-in failed");
+
+                        Toast.makeText(MainActivity.this, "Sign In Failed",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    public String getData(String key) {
+
+        return sharedPreferences.getString(key, "");
+    }
+
+    public void saveData(String key, String message) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key, message);
+        editor.apply();
     }
 
     public void loadLocation(View view) {
@@ -43,5 +140,9 @@ public class MainActivity extends AppCompatActivity {
     public void loadTraffic(View view) {
         Intent intent = new Intent(this, TrafficCameras.class);
         startActivity(intent);
+    }
+
+    public void login(View view) {
+        signIn();
     }
 }
